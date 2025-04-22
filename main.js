95fd0ecba9f86a22fac9ea48cd27762555c0f9dc
IamCosima <uzairsarvan@gmail.com>
2025-04-22 12:43:59 +0200

    Added Basic files and some basic calculator functions

---
+++ main.js
@@ -0,0 +1,17 @@
+function add(num1,num2) {
+    return num1 + num2
+      
+  };
+  
+  function subtract(num1,num2) {
+      return num1 - num2
+  };
+  
+  function multiply(num1,num2) {
+    var multiply = num1 * num2;
+    return multiply
+  };
+  
+  function divide(num1,num2){
+    return num1 / num2
+  }
