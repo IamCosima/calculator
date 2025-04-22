a8bc5d3c9701f0622edb347c6a87676fa6ba20e7
IamCosima <uzairsarvan@gmail.com>
2025-04-22 13:32:07 +0200

    Operate function test

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+var num1 = 0;
+var num2 = 0;
+var operation = "";
+
+
+function operate(num1,num2,operation){
+    if (operation == "+"){
+        add(num1,num2)
+    }else if (operation == "-"){
+        subtract(num1,num2)
+    }else if (operation == "*"){
+        multiply(num1,num2)
+    }else if (operation =="/"){
+        divide(num1,num2)
+    }else{
+        console.log("Operator Not found")
+    }
+
+}
+
+
+
 function add(num1,num2) {
     return num1 + num2
       
